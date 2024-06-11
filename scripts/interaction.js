697e17a8244d1f8742b718c12044f0ff1326e1ac
Saint <max161125@gmail.com>
2024-06-11 15:18:19 +0300

    add begin interactive fight page+rebuild

---
+++ scripts/interaction.js
@@ -0,0 +1,61 @@
+const body = document.querySelector('body');
+const changeContent = (elem, value) => {
+  elem.textContent = value;
+};
+const changeLocalStorage = (key, value) => localStorage.setItem(key, value);
+const changeKeybind = (event) => {
+  const { className } = event.currentTarget;
+  const elem = document.querySelector(`.${className.split(' ')[1]}`);
+  elem.addEventListener('keyup', (e) => {
+    const keyCode = e.code.toLowerCase();
+    if (keyCode.startsWith('key')) {
+      const key = keyCode.at(-1);
+      if (!isUsedKey(key)) {
+        changeLocalStorage(className, key);
+        changeContent(elem.firstChild, key);
+      }
+    }
+  });
+};
+const isUsedKey = (key) => {
+  for (let i = 0; i < defKeys.length; i += 1) {
+    if (localStorage.getItem(`keybind ${defKeys[i]}`) === key) {
+      return true;
+    }
+  }
+  return false;
+};
+const redrawPage = (event) => {
+  const redraw = (className, scriptArg = null) => {
+    const [target, additional = null] = className.split('_');
+    if (target === 'menu') {
+      body.classList = ['menu_bg'];
+      body.innerHTML = pagesData[className];
+      pagesData[`${className}Scripts`]();
+    } else {
+      body.classList = ['figth_stage_bg'];
+      body.innerHTML = pagesData[target];
+      pagesData[`${target}Scripts`](additional);
+    }
+  };
+  redraw(event.currentTarget.className.split(' ').at(-1));
+};
+function preload(dir, files, type) {
+  for (let i = 0; files.length > i; i += 1) {
+    img = new Image(0, 0);
+    img.src = `${dir}${files[i]}${type}`;
+    img.className += 'preload';
+    body.insertAdjacentElement('afterbegin', img);
+    body.removeChild(img);
+  }
+}
+redrawPage({ currentTarget: { className: 'menu' } });
+preload('./img/', pngTypeNames, '.png');
+Object.entries(defaultKeybinds).forEach(([key, value]) => {
+  if (!localStorage.getItem(`keybind ${key}`)) {
+    changeLocalStorage(`keybind ${key}`, value);
+  }
+});
+if (!localStorage.getItem('currentStage')) {
+  localStorage.setItem('currentStage', 1);
+}
--- scripts/pagesData.js
+++ scripts/pagesData.js
@@ -10,9 +10,9 @@ const pagesData = {
     <p class="text_menu_but">SETTINGS</p>
   </button>`,
   menuScripts() {
-    if (localStorage.getItem("currentStage") > 1) {
-      document.querySelector(".menu_newGame").insertAdjacentHTML(
-        "beforebegin",
+    if (localStorage.getItem('currentStage') > 1) {
+      document.querySelector('.menu_newGame').insertAdjacentHTML(
+        'beforebegin',
         `
       <button class="button_menu menu_continue">
         <p class="text_menu_but">CONTINUE</p>
@@ -20,42 +20,47 @@ const pagesData = {
       );
     }
     document
-      .querySelectorAll(".button_menu")
-      .forEach((button) => button.addEventListener("click", redrawHtml));
+      .querySelectorAll('.button_menu')
+      .forEach((button) => button.addEventListener('click', redrawPage));
   },
-  newGame: `
-  <a href="javascript:redrawHtml('menu')" class="text_back">&larr; Go Back</a>
+  menu_continue: `
+  <a href="javascript:redrawPage(menu)" class="text_back">&larr; Go Back</a>
   <div class="stages">
   </div>`,
-  newGameScripts() {
-    const stagesDiv = document.querySelector(".stages");
-    const createStages = (stages) => {
+  menu_continueScripts() {
+    const stagesDiv = document.querySelector('.stages');
+    const insertStages = (stages) => {
       for (let i = 0; i < stages; i += 1) {
         const stageIndex = i % 3;
         if (stageIndex === 0) {
           stagesDiv.insertAdjacentHTML(
-            "beforeend",
+            'beforeend',
             `<div class="stages_group">
             </div>`,
           );
         }
         stagesDiv.lastChild.insertAdjacentHTML(
-          "beforeend",
+          'beforeend',
           `
-          <button class="stage stage_margin stage${i + 1}">
+          <button class="stage stage_margin stage_${i + 1}">
             <span class="text_stage">STAGE ${i + 1}</span>
           </button>`,
         );
-        const stageStyle = document.getElementsByClassName(`stage${i + 1}`)[0]
-          .style;
-        stageStyle["background-size"] = "cover";
-        stageStyle["background-image"] = `url(img/stage${i + 1}.png)`;
+        const stageElem = document.getElementsByClassName(`stage_${i + 1}`)[0];
+        const stageStyle = stageElem.style;
+        stageStyle['background-size'] = 'cover';
+        stageStyle['background-image'] = `url(img/stage${i + 1}.png)`;
+        stageElem.addEventListener('click', redrawPage);
       }
     };
-    createStages(maxStage);
+    insertStages(Number(localStorage.getItem('currentStage')) + 1);
   },
-  settings: `
-  <a href="javascript:redrawHtml('menu')" class="text_back">&larr; Go Back</a>
+  menu_newGame: '',
+  menu_newGameScripts() {
+    redrawPage({ currentTarget: { className: 'stage_1' } });
+  },
+  menu_settings: `
+  <a href="javascript:redrawPage(menu)" class="text_back">&larr; Go Back</a>
   <div class="settings">
     <p class="text_control">CONTROLLS</p>
   <div class = "skills">
@@ -79,31 +84,31 @@ const pagesData = {
   </div>
   <div class="set_keybind">
     <p class="text_binding">
-  Set keybinds
-  <button class="keybinds_reset"><span class="text_reset">RESET</span></button>
-  </p>
-  <button class="keybind water"><span class="text_bin_key"></span></button>
-  <button class="keybind fire"><span class="text_bin_key"></span></button>
-  <button class="keybind air"><span class="text_bin_key"></span></button>
-  <button class="keybind earth"><span class="text_bin_key"></span></button>
-  </div>
+    Set keybinds
+    <button class="keybinds_reset"><span class="text_reset">RESET</span></button>
+    </p>
+      <button class="keybind water"><span class="text_bin_key"></span></button>
+      <button class="keybind fire"><span class="text_bin_key"></span></button>
+      <button class="keybind air"><span class="text_bin_key"></span></button>
+      <button class="keybind earth"><span class="text_bin_key"></span></button>
+    </div>
   </div>`,
-  settingsScripts() {
-    const buttons = document.querySelectorAll(".keybind");
+  menu_settingsScripts() {
+    const buttons = document.querySelectorAll('.keybind');
     buttons.forEach((button) => {
-      const name = button.className;
-      if (!localStorage.getItem(name)) {
-        changeLocalStorage(name, defaultKeys[name.split(" ")[1]]);
+      const { className } = button;
+      const skillName = className.split(' ')[1];
+      if (!localStorage.getItem(className)) {
+        changeLocalStorage(className, defaultKeybinds[skillName]);
       }
       changeContent(
         button.firstChild,
-        localStorage.getItem(button.className) ??
-          defaultKeys[button.className.split(" ")[1]],
+        localStorage.getItem(className) ?? defaultKeybinds[skillName],
       );
-      button.addEventListener("click", (event) => changeKeybind(event));
+      button.addEventListener('click', (event) => changeKeybind(event));
     });
     const resetKeybinds = () => {
-      Object.entries(defaultKeys).forEach(([key, value]) => {
+      Object.entries(defaultKeybinds).forEach(([key, value]) => {
         changeLocalStorage(`keybind ${key}`, value);
         changeContent(
           document.querySelector(`.keybind.${key}`).firstElementChild,
@@ -112,22 +117,93 @@ const pagesData = {
       });
     };
     document
-      .querySelector(".keybinds_reset")
-      .addEventListener("click", resetKeybinds);
+      .querySelector('.keybinds_reset')
+      .addEventListener('click', resetKeybinds);
   },
-  continue: `
-  <a href="javascript:redrawHtml('menu')" class="text_back">&larr; Go Back</a>`,
-  continueScripts() {},
+  stage: `
+    <a href="javascript:redrawPage(menu)" class="text_back">&larr; Go Back</a>
+    <div class="cont">
+      <div class="hp enemy-hp"></div>
+      <div class="hp enemy-hp2"></div>
+      <div class="interaction">
+        <div class="fight_skills">
+          <div class="skill_position water">
+            <img src="img/skill_water.png" class ="skill_img"/>
+            <p class="fight_skills_text"></p>
+          </div>
+          <div class="skill_position fire">
+            <img src="img/skill_fire.png" class ="skill_img"/>
+            <p class="fight_skills_text"></p>
+          </div>
+          <div class="skill_position earth">
+            <img src="img/skill_earth.png" class ="skill_img"/>
+            <p class="fight_skills_text"></p>
+          </div>
+          <div class="skill_position air">
+            <img src="img/skill_air.png" class ="skill_img"/>
+            <p class="fight_skills_text"></p>
+          </div>
+        </div>
+        <div class="arrows">
+          <img src="img/arrow_horizon.png" class="arrow_left" />
+          <img src="img/arrow_vertical.png" class="arrow_top" />
+          <img src="img/arrow_vertical.png" class="arrow_bottom" />
+          <img src="img/arrow_horizon.png" class="arrow_right" />
+        </div>
+      </div>
+    </div>
+  `,
+  stageScripts(number) {
+    body.style['background-image'] = `url(img/stage${number}.png)`;
+    defKeys.forEach((skillName) => {
+      const skillDiv = document.getElementsByClassName(skillName)[0];
+      const skillP = skillDiv.querySelector('p');
+      skillP.innerText = localStorage.getItem(`keybind ${skillName}`);
+    });
+    function changeBorder(skillName) {
+      Object.values(document.getElementsByClassName('skill_img')).forEach(
+        (e) => {
+          e.style.borderColor = '#381015';
+        },
+      );
+      document
+        .getElementsByClassName(skillName)[0]
+        .querySelector('img').style.borderColor = 'rgb(236, 236, 61)';
+    }
+    function chooseSkill(event) {
+      const codeKey = event.code.toLowerCase();
+      const keys = Object.keys(defaultKeybinds);
+      for (let i = 0; i < keys.length; i += 1) {
+        const skillName = keys[i];
+        if (
+          codeKey.startsWith('key')
+          && codeKey.at(-1) === localStorage.getItem(`keybind ${skillName}`)
+        ) {
+          changeBorder(skillName);
+        }
+      }
+    }
+    document.addEventListener('keyup', chooseSkill);
+  },
 };
 
-const defaultKeys = {
-  water: "q",
-  fire: "w",
-  air: "e",
-  earth: "r",
+const defaultKeybinds = {
+  water: 'q',
+  fire: 'w',
+  air: 'e',
+  earth: 'r',
 };
+const defKeys = Object.keys(defaultKeybinds);
 const maxStage = 9;
-const pngTypeNames = ["skill_air", "skill_earth", "skill_fire", "skill_water"];
+const pngTypeNames = [
+  'skill_air',
+  'skill_earth',
+  'skill_fire',
+  'skill_water',
+  'arrow_horizon',
+  'arrow_vertical',
+];
 for (let i = 0; i < maxStage; i += 1) {
   pngTypeNames.push(`stage${i + 1}`);
 }
+const menu = { currentTarget: { className: 'menu' } };
